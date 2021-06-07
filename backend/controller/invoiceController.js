eab24432fb9487b19906b09e1dc005b61f74ef02
Renszo Camacho <varsovia84@gmail.com>
2021-06-07 17:48:49 +0200

    Create getAll function in the invoice controller

--- backend/controller/invoiceController.js
+++ backend/controller/invoiceController.js
@@ -1,6 +1,18 @@
 const Invoice = require('../models/invoiceModel');
 
 const invoiceController = () => {
+  const getAll = async (req, res) => {
+    try {
+      const invoice = await Invoice.find(req.query);
+      res.json(invoice);
+    } catch (error) {
+      res.status(500);
+    }
+  };
+
+  return {
+    getAll
+  };
 };
 
 module.exports = invoiceController;
