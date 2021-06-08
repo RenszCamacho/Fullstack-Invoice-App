36cb3ef957a01f4528a767810d031128caca0d57
Renszo Camacho <varsovia84@gmail.com>
2021-06-08 19:21:14 +0200

    Create actionCreators - getInvoices - addInvoice - updateInvoice - deleteInvoice

---
+++ frontend/src/redux/actions/actionCreators.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-underscore-dangle */
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const invoicesUrl = process.env.REACT_APP_API_URL;
+
+export function getInvoices() {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(invoicesUrl);
+      dispatch({
+        type: actionTypes.GET_ALL_INVOICES,
+        invoices: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.GET_ALL_INVOICES_ERROR
+      });
+    }
+  };
+}
+
+export function addInvoice(invoice) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(invoicesUrl, invoice);
+      dispatch({
+        type: actionTypes.CREATE_INVOICE,
+        invoices: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.CREATE_INVOICE_ERROR
+      });
+    }
+  };
+}
+
+export function updateInvoice(invoice) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`${invoicesUrl}${invoice._id}`, invoice);
+      dispatch({
+        type: actionTypes.UPDATE_INVOICE,
+        invoices: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.UPDATE_INVOICE_ERROR
+      });
+    }
+  };
+}
+
+export function deleteInvoice(invoiceId) {
+  return async (dispatch) => {
+    try {
+      await axios.delete(`${invoicesUrl}${invoiceId}`);
+      dispatch({
+        type: actionTypes.DELETE_INVOICE,
+        invoiceId
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.DELETE_INVOICE_ERROR
+      });
+    }
+  };
+}
