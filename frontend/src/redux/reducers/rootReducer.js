4881d4f23f46c52283eb0a73f0f69c3df2f0bc75
Renszo Camacho <varsovia84@gmail.com>
2021-06-09 09:26:46 +0200

    Create rootReducer

---
+++ frontend/src/redux/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import invoicesReducer from './invoicesReducer';
+
+const rootReducer = combineReducers({
+  invoices: invoicesReducer
+});
+
+export default rootReducer;
