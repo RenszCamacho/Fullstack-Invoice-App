b2df96d37eb73bda4543d0ed03b4c3a11823a49d
Renszo Camacho <varsovia84@gmail.com>
2021-06-09 20:40:56 +0200

    Create Avatar component

---
+++ frontend/src/components/Avatar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import avatar from './avatar.png';
+
+function Avatar() {
+  return (
+    <div>
+      <img src={avatar} alt="avatar" />
+    </div>
+  );
+}
+
+export default Avatar;
