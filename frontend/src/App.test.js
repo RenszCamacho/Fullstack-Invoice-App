b48d486631727d3f740e9ed57d59491553058e47
Renszo Camacho <varsovia84@gmail.com>
2021-06-10 16:00:15 +0200

    Add App.test.js

---
+++ frontend/src/App.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+test('renders Invoices App', () => {
+  render(<App />);
+  const dashboard = screen.getByText(/dashboard/i);
+  expect(dashboard).toBeInTheDocument();
+});
