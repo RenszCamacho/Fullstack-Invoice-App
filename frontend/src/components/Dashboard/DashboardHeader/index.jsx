bc39fc649f277a9644d9ffc6a827b60b441f8625
Renszo Camacho <varsovia84@gmail.com>
2021-06-10 20:49:48 +0200

    Create DashboardHeader

---
+++ frontend/src/components/Dashboard/DashboardHeader/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../../Button';
+
+function DashboardHeader({ message }) {
+  return (
+    <div>
+      <div>
+        <h1>Invoices</h1>
+        <span>{message}</span>
+      </div>
+      <div>
+        <button
+          type="button"
+          aria-label="drop down"
+        >
+          <span>Filter</span>
+          <em className="fas fa-chevron-down" />
+        </button>
+        <div>
+          <Button nameBtn="New" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+DashboardHeader.propTypes = {
+  message: PropTypes.element.isRequired
+};
+
+export default DashboardHeader;
