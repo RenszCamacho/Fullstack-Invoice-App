f5107d009494e9f09e539c2d1646baf7418e3a44
Renszo Camacho <varsovia84@gmail.com>
2021-06-10 20:49:59 +0200

    Create Dashboard

---
+++ frontend/src/components/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DashboardHeader from './DashboardHeader';
+
+function Dashboard() {
+  return (
+    <main>
+      <DashboardHeader message="0 Invoices" />
+
+    </main>
+  );
+}
+
+export default Dashboard;
