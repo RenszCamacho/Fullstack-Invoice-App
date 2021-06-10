034994ef3ddff757c4a7abcb26aa7e5950f0354e
Renszo Camacho <varsovia84@gmail.com>
2021-06-10 20:58:55 +0200

    Add Link component in the Logo coponent

--- frontend/src/components/Header/Logo/index.jsx
+++ frontend/src/components/Header/Logo/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 
 function Logo() {
   return (
     <div className="header-container__logo">
-      <img className="logo__image" src={logo} alt="logo" />
+      <Link to="/">
+        <img className="logo__image" src={logo} alt="logo" />
+      </Link>
     </div>
   );
 }
