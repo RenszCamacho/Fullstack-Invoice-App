18d0f959944a30673676b9a4842cb2d2cf075e4f
Renszo Camacho <varsovia84@gmail.com>
2021-06-12 10:28:11 +0200

    - Add className in the DashboardHeader

--- frontend/src/components/Dashboard/DashboardHeader/index.jsx
+++ frontend/src/components/Dashboard/DashboardHeader/index.jsx
@@ -2,31 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../Button';
 
-function DashboardHeader({ message }) {
+function DashboardHeader({ invoices }) {
   return (
-    <div>
-      <div>
-        <h1>Invoices</h1>
-        <span>{message}</span>
+    <div className="dashboard-container__dashboard-header">
+      <div className="dashboard-header__description">
+        <h1 className="description__title">Invoices</h1>
+        <span className="description__invoices">{invoices}</span>
       </div>
+      <button
+        className="dashboard-header__btn"
+        type="button"
+        aria-label="drop down"
+      >
+        <span>Filter</span>
+        <em className="fas fa-chevron-down" />
+      </button>
       <div>
-        <button
-          type="button"
-          aria-label="drop down"
-        >
-          <span>Filter</span>
-          <em className="fas fa-chevron-down" />
-        </button>
-        <div>
-          <Button nameBtn="New" />
-        </div>
+        <Button nameBtn="New" />
       </div>
     </div>
   );
 }
 
 DashboardHeader.propTypes = {
-  message: PropTypes.string.isRequired
+  invoices: PropTypes.string.isRequired
 };
 
 export default DashboardHeader;
