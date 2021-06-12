1ca221df13685a35afcb4bfb305654dd23fe79c6
Renszo Camacho <varsovia84@gmail.com>
2021-06-12 10:29:04 +0200

    Render and Style Dashboard component

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import DashboardHeader from './DashboardHeader';
+import { getInvoices } from '../../redux/actions/actionCreators';
+import InvoiceItem from './InvoiceItem';
+import './dashboard.scss';
 
 function Dashboard() {
-  return (
-    <main>
-      <DashboardHeader message="0 Invoices" />
+  const dispatch = useDispatch();
+  const invoices = useSelector((store) => store.invoices);
 
+  useEffect(() => {
+    dispatch(getInvoices());
+  }, []);
+
+  return (
+    <main className="dashboard-container">
+      <DashboardHeader
+        invoices={`There are ${invoices.length} invoices`}
+      />
+
+      <ul className="dashboard-container__list">
+        {
+          invoices.map(
+            (invoice) => (
+              <Link to="/">
+                <InvoiceItem
+                  key={invoice._id}
+                  idNumber={
+                  invoice._id
+                    .toUpperCase()
+                    .slice(-5)
+                }
+                  dueDate={invoice.projectDescription}
+                  name={invoice.to.name}
+                  total={invoice.total}
+                />
+              </Link>
+            )
+          )
+        }
+      </ul>
     </main>
   );
 }
