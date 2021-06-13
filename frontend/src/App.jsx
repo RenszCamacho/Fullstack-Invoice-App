277844970305032e07bac9cc7f287813c6ef9e61
Renszo Camacho <varsovia84@gmail.com>
2021-06-13 10:39:20 +0200

    Remove Header component from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,12 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Dashboard from './components/Dashboard';
-import Header from './components/Header';
 import configureStore from './redux/store';
 
 function App() {
   return (
     <Provider store={configureStore()}>
       <Router>
-        <Header />
         <Switch>
           <Route path="/" exact component={Dashboard} />
         </Switch>
