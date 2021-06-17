abb20dbf118254df1a01f0813ea89e1d366625f6
Renszo Camacho <varsovia84@gmail.com>
2021-06-17 21:50:48 +0200

    Modify ThemeButtom

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Avatar from './Avatar';
 import Logo from './Logo';
-import ThemeButton from './ThemeButton/index';
 import './header.scss';
 
 function Header() {
   return (
     <header className="header-container">
       <Logo />
-      <ThemeButton />
       <Avatar />
     </header>
   );
