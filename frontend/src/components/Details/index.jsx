75e3bcea7cae720b14b5d389d3f9f71b23cb7577
Renszo Camacho <varsovia84@gmail.com>
2021-06-20 15:02:38 +0200

    Set showEditForm on the Detail component

--- frontend/src/components/Details/index.jsx
+++ frontend/src/components/Details/index.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import dayjs from 'dayjs';
-import { useParams, useHistory, withRouter } from 'react-router-dom';
+import { useParams, /* useHistory */ withRouter } from 'react-router-dom';
 import { getOneInvoice, markAsPaid /* updateInvoice */ } from '../../redux/actions/actionCreators';
 import Header from '../Header';
+import EditForm from '../Forms/EditForm';
 import multiply from '../../services/multiply';
 import currencyFormat from '../../services/currencyFormat';
 import RegularBtn from '../Buttons/RegularBtn';
@@ -17,8 +18,9 @@ function Details() {
   const invoice = useSelector((store) => store.invoice);
 
   const [showModal, setShowModal] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
   const { invoiceId } = useParams();
-  const history = useHistory();
+  // const history = useHistory();
 
   useEffect(() => {
     dispatch(getOneInvoice(invoiceId));
@@ -34,93 +36,102 @@ function Details() {
     setShowModal((previous) => !previous);
   }
 
-  function redirect() {
-    history.push('/editform');
+  function openEditForm() {
+    setShowEditForm((previous) => !previous);
   }
+
+  // function redirect() {
+  //   history.push('/editform');
+  // }
+
   // function editInvoice() {
   //   dispatch(updateInvoice(invoice));
   // }
 
   return (
     <>
-      { invoice?._id && (
+      {
+        showEditForm
+          ? <EditForm />
+          : (
+            invoice?._id && (
 
-      <main className="details-container">
-        <Modal
-          showModal={showModal}
-          setShowModal={setShowModal}
-          invoice={invoice?._id}
-          id={
+            <main className="details-container">
+              <Modal
+                showModal={showModal}
+                setShowModal={setShowModal}
+                invoice={invoice?._id}
+                id={
               invoice?._id
                 .toUpperCase()
                 .slice(-5)
             }
-        />
+              />
 
-        <Header />
+              <Header />
 
-        <GoBack />
+              <GoBack />
 
-        <div className="details-container__status">
-          <p className="status__paragraph">Status</p>
-          <div className={invoice?.status ? 'status__paid-status' : 'status__pending-status'}>
-            <em className="fas fa-circle" />
-            {invoice?.status ? 'Paid' : 'Pending'}
-          </div>
-        </div>
+              <div className="details-container__status">
+                <p className="status__paragraph">Status</p>
+                <div className={invoice?.status ? 'status__paid-status' : 'status__pending-status'}>
+                  <em className="fas fa-circle" />
+                  {invoice?.status ? 'Paid' : 'Pending'}
+                </div>
+              </div>
 
-        <div className="details-container__body">
-          <div className="body__project-description">
-            #
-            <span>
-              {invoice?._id
-                .slice(-5)
-                .toUpperCase()}
-            </span>
-            <h2>{invoice?.projectDescription}</h2>
-          </div>
+              <div className="details-container__body">
+                <div className="body__project-description">
+                  #
+                  <span>
+                    {invoice?._id
+                      .slice(-5)
+                      .toUpperCase()}
+                  </span>
+                  <h2>{invoice?.projectDescription}</h2>
+                </div>
 
-          <address className="body__address-to">
-            {invoice?.from?.address?.street}
-            <br />
-            {invoice?.from?.address?.city}
-            <br />
-            {invoice?.from?.address?.postCode}
-            <br />
-            {invoice?.from?.address?.country}
-            <br />
-          </address>
+                <address className="body__address-to">
+                  {invoice?.from?.address?.street}
+                  <br />
+                  {invoice?.from?.address?.city}
+                  <br />
+                  {invoice?.from?.address?.postCode}
+                  <br />
+                  {invoice?.from?.address?.country}
+                  <br />
+                </address>
 
-          <div className="body__invoice-date">
-            <h3>Invoice Date</h3>
-            <span>{dayjs(invoice?.invoiceDate).format('DD MMM YYYY')}</span>
-          </div>
+                <div className="body__invoice-date">
+                  <h3>Invoice Date</h3>
+                  <span>{dayjs(invoice?.invoiceDate).format('DD MMM YYYY')}</span>
+                </div>
 
-          <div className="body__payment-due">
-            <h3>Paymet Due</h3>
-            <span>{dayjs(invoice?.paymentTerms).format('DD MMM YYYY')}</span>
-          </div>
+                <div className="body__payment-due">
+                  <h3>Paymet Due</h3>
+                  <span>{dayjs(invoice?.paymentTerms).format('DD MMM YYYY')}</span>
+                </div>
 
-          <div className="body__sent-to">
-            <h3>Sent to</h3>
-            <span>{invoice?.to?.email}</span>
-          </div>
+                <div className="body__sent-to">
+                  <h3>Sent to</h3>
+                  <span>{invoice?.to?.email}</span>
+                </div>
 
-          <address className="body__address-from">
-            <p>Bill to</p>
-            <h3>{invoice?.to?.name}</h3>
-            {invoice?.to?.address?.street}
-            <br />
-            {invoice?.to?.address?.city}
-            <br />
-            {invoice?.to?.address?.postCode}
-            <br />
-            {invoice?.to?.address?.country}
-            <br />
-          </address>
+                <address className="body__address-from">
+                  <p>Bill to</p>
+                  <h3>{invoice?.to?.name}</h3>
+                  {invoice?.to?.address?.street}
+                  <br />
+                  {invoice?.to?.address?.city}
+                  <br />
+                  {invoice?.to?.address?.postCode}
+                  <br />
+                  {invoice?.to?.address?.country}
+                  <br />
+                </address>
 
-          <ul className="body__list">
-            {
+                <ul className="body__list">
+                  {
                 invoice.items.map(
                   (item) => (
                     <li className="list__item-details" key={`${item._id}A`}>
@@ -133,38 +144,39 @@ function Details() {
                   )
                 )
               }
-            <div className="list__total">
-              <p>Grand Total</p>
-              <p>{currencyFormat(grandTotal(invoice?.items))}</p>
-            </div>
-          </ul>
+                  <div className="list__total">
+                    <p>Grand Total</p>
+                    <p>{currencyFormat(grandTotal(invoice?.items))}</p>
+                  </div>
+                </ul>
 
-        </div>
-        <div className="details-container__btn">
+              </div>
+              <div className="details-container__btn">
 
-          <RegularBtn
-            nameBtn="Edit"
-            modify="info"
-            onClick={redirect}
-            // onClick={editInvoice}
-          />
+                <RegularBtn
+                  nameBtn="Edit"
+                  modify="info"
+                  onClick={openEditForm}
+                />
 
-          <RegularBtn
-            nameBtn="Delete"
-            modify="danger"
-            onClick={openModal}
-          />
+                <RegularBtn
+                  nameBtn="Delete"
+                  modify="danger"
+                  onClick={openModal}
+                />
 
-          {!invoice?.status && (
-          <RegularBtn
-            modify="primary"
-            nameBtn="Mark as Paid"
-            onClick={toggleStatus}
-          />
-          )}
-        </div>
-      </main>
-      )}
+                {!invoice?.status && (
+                <RegularBtn
+                  modify="primary"
+                  nameBtn="Mark as Paid"
+                  onClick={toggleStatus}
+                />
+                )}
+              </div>
+            </main>
+            )
+          )
+      }
     </>
   );
 }
