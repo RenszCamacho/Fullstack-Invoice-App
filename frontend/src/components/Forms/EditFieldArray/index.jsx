7663dde6dcdbc724aebef6f0eeff26b4f5c11cbb
Renszo Camacho <varsovia84@gmail.com>
2021-06-20 21:19:29 +0200

    Create a new FieldArray component

---
+++ frontend/src/components/Forms/EditFieldArray/index.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { useFieldArray } from 'react-hook-form';
+
+export default function Fields({
+  register, control, getValues, setValue
+}) {
+  const lastInput = {};
+
+  function multiply(index) {
+    const lastPrice = getValues(`items.${index}.price`);
+    const lastQuantity = getValues(`items.${index}.quantity`);
+    lastInput[index] = lastQuantity * (+lastPrice);
+    return lastInput[index];
+  }
+  const {
+    fields, append, remove
+  } = useFieldArray({
+    control,
+    name: 'items'
+  });
+
+  return (
+    <>
+      <ul className="fieldset-items__list-items">
+        {fields.map((item, index) => (
+          <li className="list-items__item" key={item.id}>
+
+            <label className="item__label" htmlFor="itemList">
+              Item Name
+              <input
+                defaultValue={item.name}
+                {...register(`items[${index}].name`)}
+                placeholder="Product Name"
+              />
+            </label>
+
+            <label className="item__label" htmlFor="itemList">
+              Qty.
+              <input
+                {...register(`items[${index}].quantity`)}
+                placeholder="Qty"
+              />
+
+            </label>
+
+            <label className="item__label" htmlFor="itemList">
+              Price
+              <input
+                {...register(`items[${index}].price`)}
+                placeholder="Price"
+              />
+            </label>
+
+            <label className="item__label item__label--total" htmlFor="itemList">
+              Total
+              <input
+                {...register(`items[${index}].total`)}
+              />
+            </label>
+
+            <button className="item__btn" type="button" onClick={() => remove(index)}>
+              <em className="fas fa-trash" />
+            </button>
+
+            <button
+              className="fieldset-items__btn"
+              type="button"
+              onClick={
+              () => setValue(`items[${index}].total`, multiply(index))
+              }
+            >
+              Get Total
+            </button>
+          </li>
+
+        ))}
+      </ul>
+
+      <button
+        className="fieldset-items__btn"
+        type="button"
+        onClick={() => {
+          append({ name: '' });
+        }}
+      >
+        + Add New Item
+      </button>
+
+    </>
+  );
+}
